6153cc8799808216daa95678e9a44c438e14dbe7
Tsegaye16 <abewatsegaye16@gmail.com>
2023-11-07 13:19:18 -0800

    dhjadsa

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-'''Module '''
+"""Module for task 0"""
 
 
 def number_of_subscribers(subreddit):
-    '''The function that queries the Reddit API and
-	returns the number of subscribers'''
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
     import requests
 
     sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
