21b18cbf10fd89f63de3c9f6c13f1a4672da0c12
Tsegaye16 <abewatsegaye16@gmail.com>
2023-11-07 13:41:20 -0800

    djdsas

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''last module for mandatory task'''
+
+def recurse(subreddit, hot_list=[], count=0, after=None):
+    '''a recursive function that queries the Reddit API and returns
+    a list containing the titles of all hot articles for a given subreddit'''
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
+                            .format(subreddit),
+                            params={"count": count, "after": after},
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 400:
+        return None
+
+    hot_l = hot_list + [child.get("data").get("title")
+                        for child in sub_info.json()
+                        .get("data")
+                        .get("children")]
+
+    info = sub_info.json()
+    if not info.get("data").get("after"):
+        return hot_l
+
+    return recurse(subreddit, hot_l, info.get("data").get("count"),
+                   info.get("data").get("after"))
