a0d25c8718426300b13abac452ccecbecbdb0e9c
Tsegaye16 <abewatsegaye16@gmail.com>
2023-11-07 13:44:13 -0800

    dgshjdas

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-'''last module for mandatory task'''
+"""Module for task 2"""
+
 
 def recurse(subreddit, hot_list=[], count=0, after=None):
-    '''a recursive function that queries the Reddit API and returns
-    a list containing the titles of all hot articles for a given subreddit'''
+    """Queries the Reddit API and returns all hot posts
+    of the subreddit"""
     import requests
 
     sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
